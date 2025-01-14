98cd66b2a79e3277992b955372244f3eff966403
snb13 <77163124+snb13@users.noreply.github.com>
2025-01-14 10:03:38 +0300

    task 2 done

---
+++ task2/src/Task2.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Task2 {
+    public static void main(String[] args) throws IOException {
+        File circle = new File(args[0]);
+        File point = new File(args[1]);
+        Scanner sc1 = new Scanner(new FileInputStream(circle));
+        Scanner sc2 = new Scanner(new FileInputStream(point));
+        ArrayList<Integer> arrList = new ArrayList<>();
+        ArrayList<Integer> pointList = new ArrayList<>();
+        int result;
+
+        while (sc1.hasNextInt()) {
+            arrList.add(sc1.nextInt());
+        }
+        while (sc2.hasNextInt()) {
+            pointList.add(sc2.nextInt());
+        }
+
+        int x = arrList.get(0);
+        int y = arrList.get(1);
+        int rad = arrList.get(2);
+
+
+        for (int i = 0; i < pointList.size() - 1; i += 2) {
+
+            int x1_coord = pointList.get(i);
+            int y1_coord = pointList.get(i+1);
+
+            if (x == x1_coord && Math.abs(y1_coord - y) == rad || y == y1_coord && Math.abs(x1_coord - x) == rad) {
+                result = 0;
+            } else if (x1_coord >= Math.abs(x + rad) || y1_coord >= Math.abs(y + rad)){
+                result = 2;
+            } else {
+                result = 1;
+            }
+
+            System.out.println(result);
+        }
+
+    }
+
+}

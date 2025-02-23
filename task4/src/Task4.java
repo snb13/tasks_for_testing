9c6e05f7199d4533dfe1442386f09639a8d10357
snb13 <77163124+snb13@users.noreply.github.com>
2025-02-24 02:20:20 +0300

    task 4 updated

--- task4/src/Task4.java
+++ task4/src/Task4.java
@@ -4,46 +4,31 @@ import java.util.Scanner;
 
 public class Task4 {
 
+
     public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.out.println("Не указан файл с координатами.");
+            return;
+        }
+
         File coordsFile = new File(args[0]);
         Scanner scanner = new Scanner(new FileInputStream(coordsFile));
         ArrayList<Integer> arrList = new ArrayList<>();
-
 
         while (scanner.hasNextInt()) {
             arrList.add(scanner.nextInt());
         }
 
-        int tempNum = (arrList.stream()
-                .mapToInt(num -> num)
-                .sum());
-        double t = (double) tempNum / arrList.size();
+        double sum = arrList.stream().mapToInt(Integer::intValue).sum();
+        double average = sum / arrList.size();
+
+        int resultNum = (int) Math.round(average);
 
         int counter = 0;
-
-        int resultNum;
-        if ((t % (int) t) >= 0.5) {
-                resultNum = (int) Math.ceil(t);
-        }
-        else if (t >= 0.5 && t < 1.0) {
-                resultNum = 1;
-        } else  {
-            resultNum = (int) Math.floor(t);
-        }
-
-
-
         for (int m = 0; m < arrList.size(); m++) {
-            while (arrList.get(m) != resultNum) {
-                if (arrList.get(m) < resultNum) {
-                    arrList.set(m, arrList.get(m) + 1);
-                }
-                if (arrList.get(m) > resultNum) {
-                    arrList.set(m, arrList.get(m) - 1);
-                }
-                counter++;
-            }
+            counter += Math.abs(arrList.get(m) - resultNum);
         }
+
         System.out.println(counter);
     }
 }
